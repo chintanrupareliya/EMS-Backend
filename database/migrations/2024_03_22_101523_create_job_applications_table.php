5e60115ae533813e2d663e4cd3001535865286cc
chintanrupareliya <ckrupareliya2002@gmail.com>
2024-04-24 23:36:52 +0530

    add: fields in job applications

--- database/migrations/2024_03_22_101523_create_job_applications_table.php
+++ database/migrations/2024_03_22_101523_create_job_applications_table.php
@@ -11,22 +11,24 @@ return new class extends Migration {
     public function up(): void
     {
         Schema::create('job_applications', function (Blueprint $table) {
+
             $table->id();
             $table->unsignedBigInteger('user_id');
-            $table->foreign('user_id')->references('id')->on('users');
-
             $table->unsignedBigInteger('job_id');
-            $table->foreign('job_id')->references('id')->on('jobs');
-
-            $table->string('resume_path');
-            $table->text('cover_letter')->nullable();
-            $table->string('application_status');
+            $table->longText('resume');
+            $table->text('cover_letter');
+            $table->enum('status', ['P', 'A', 'R'])->default('P');
+            $table->text('comment')->nullable();
+            $table->timestamp('application_date')->nullable();
 
             $table->unsignedBigInteger('created_by')->nullable();
             $table->unsignedBigInteger('updated_by')->nullable();
             $table->unsignedBigInteger('deleted_by')->nullable();
             $table->softDeletes();
             $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('job_id')->references('id')->on('jobs')->onDelete('cascade');
         });
     }
 
